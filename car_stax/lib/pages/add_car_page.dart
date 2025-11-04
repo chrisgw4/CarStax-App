08d9b8157db03e439bda49b9af202d80fb3700ab
Christian <chrisgw4@gmail.com>
2025-11-04 08:37:31 -0500

    Added add car page

---
+++ car_stax/lib/pages/add_car_page.dart
@@ -0,0 +1,336 @@
+import 'dart:ffi';
+
+import 'package:car_stax/components/my_text.dart';
+import 'package:car_stax/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+typedef CarEntry = DropdownMenuEntry<CarType>;
+
+// DropdownMenuEntry labels and values for the second dropdown menu.
+enum CarType {
+  sedan(
+    'Sedan',
+    NetworkImage("https://farrukhanwar.site/assets/sedan-BfLnftng.png"),
+  ),
+  suv('SUV', NetworkImage("https://farrukhanwar.site/assets/suv-DHEXZqaC.png")),
+  truck(
+    'Truck',
+    NetworkImage("https://farrukhanwar.site/assets/truck-DWk2EocT.png"),
+  ),
+  coupe(
+    'Coupe',
+    NetworkImage("https://farrukhanwar.site/assets/coupe-DpR66DVc.png"),
+  ),
+  convertible(
+    'Convertible',
+    NetworkImage("https://farrukhanwar.site/assets/convertible-D5rVBvli.png"),
+  ),
+  hatchback(
+    'Hatchback',
+    NetworkImage("https://farrukhanwar.site/assets/hatchback-Bz_gbJNw.png"),
+  ),
+  van('Van', NetworkImage("https://farrukhanwar.site/assets/van-D86uPqS3.png")),
+  motorcycle(
+    'Motorcycle',
+    NetworkImage("https://farrukhanwar.site/assets/motorcycle-BLOPTAqK.png"),
+  ),
+  other(
+    'Other',
+    NetworkImage("https://farrukhanwar.site/assets/other-Db6NQrY2.png"),
+  );
+
+  const CarType(this.label, this.icon);
+  final String label;
+  final NetworkImage icon;
+
+  static final List<CarEntry> entries = UnmodifiableListView<CarEntry>(
+    values.map<CarEntry>(
+      (CarType icon) => CarEntry(
+        value: icon,
+        label: icon.label,
+        leadingIcon: ImageIcon(icon.icon),
+      ),
+    ),
+  );
+}
+
+typedef RentalEntry = DropdownMenuEntry<RentalStatus>;
+
+// DropdownMenuEntry labels and values for the second dropdown menu.
+enum RentalStatus {
+  available(
+    'Available',
+    NetworkImage("https://farrukhanwar.site/assets/available-C_NG3xW-.png"),
+  ),
+  rented(
+    'Rented',
+    NetworkImage("https://farrukhanwar.site/assets/rented-CFektGni.png"),
+  ),
+  maintenance(
+    'Maintenance',
+    NetworkImage("https://farrukhanwar.site/assets/maintenance-CUMVvKMO.png"),
+  );
+
+  const RentalStatus(this.label, this.icon);
+  final String label;
+  final NetworkImage icon;
+
+  static final List<RentalEntry> entries = UnmodifiableListView<RentalEntry>(
+    values.map<RentalEntry>(
+      (RentalStatus icon) => RentalEntry(
+        value: icon,
+        label: icon.label,
+        leadingIcon: ImageIcon(icon.icon),
+      ),
+    ),
+  );
+}
+
+class AddCarPage extends StatefulWidget {
+  const AddCarPage({super.key});
+
+  @override
+  State<AddCarPage> createState() => _AddCarPageState();
+}
+
+class _AddCarPageState extends State<AddCarPage> {
+  final TextEditingController lPLateController = TextEditingController();
+
+  final TextEditingController yearController = TextEditingController();
+
+  final TextEditingController mileageController = TextEditingController();
+
+  final TextEditingController makeController = TextEditingController();
+
+  final TextEditingController modelController = TextEditingController();
+
+  final TextEditingController colorController = TextEditingController();
+
+  final TextEditingController vinController = TextEditingController();
+
+  final TextEditingController carTypeController = TextEditingController();
+
+  final TextEditingController rentalStatusController = TextEditingController();
+  CarType? selectedCar;
+  RentalStatus? selectedStatus;
+  late List<TextEditingController> warningList = [];
+
+
+  @override
+  Widget build(BuildContext context) {
+    print('Number of entries: ${CarType.entries.length}');
+    for (var entry in CarType.entries) {
+      print('Entry label: ${entry.label}, value: ${entry.value}');
+    }
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.only(left: 48, right: 48),
+            child: Column(
+              // Centers Text
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("License Plate", style: TextStyle(fontSize: 16)),
+                MyTextField(
+                  hintText: "ABC-1234",
+                  obscureText: false,
+                  controller: lPLateController,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Year", style: TextStyle(fontSize: 16)),
+                          MyTextField(
+                            hintText: "2025",
+                            obscureText: false,
+                            controller: yearController,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Mileage", style: TextStyle(fontSize: 16)),
+                          MyTextField(
+                            hintText: "0",
+                            obscureText: false,
+                            controller: mileageController,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Make", style: TextStyle(fontSize: 16)),
+                          MyTextField(
+                            hintText: "Toyota",
+                            obscureText: false,
+                            controller: makeController,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Model", style: TextStyle(fontSize: 16)),
+                          MyTextField(
+                            hintText: "Camry",
+                            obscureText: false,
+                            controller: modelController,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text("Color", style: TextStyle(fontSize: 16)),
+                MyTextField(
+                  hintText: "Black",
+                  obscureText: false,
+                  controller: colorController,
+                ),
+                SizedBox(height: 10),
+                Text("VIN Number", style: TextStyle(fontSize: 16)),
+                MyTextField(
+                  hintText: "1A2BC34567D890123",
+                  obscureText: false,
+                  controller: vinController,
+                ),
+                SizedBox(height: 10),
+                Text("Car Type", style: TextStyle(fontSize: 16)),
+
+                DropdownMenu<CarType>(
+                  controller: carTypeController,
+
+                  textAlign: TextAlign.center,
+                  width: 200,
+
+                  inputDecorationTheme: const InputDecorationTheme(
+                    filled: true,
+
+                    prefixIconConstraints: BoxConstraints(
+                      minHeight: 40,
+                      minWidth: 40,
+                    ),
+
+                    contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+                  ),
+                  onSelected: (CarType? icon) {
+                    setState(() {
+                      selectedCar = icon;
+                    });
+                  },
+                  dropdownMenuEntries: CarType.entries,
+                  leadingIcon: ImageIcon(selectedCar?.icon),
+                ),
+                SizedBox(height: 10),
+                Text("Rental Status", style: TextStyle(fontSize: 16)),
+                DropdownMenu<RentalStatus>(
+                  controller: rentalStatusController,
+
+                  textAlign: TextAlign.center,
+                  width: 200,
+
+                  inputDecorationTheme: const InputDecorationTheme(
+                    filled: true,
+
+                    prefixIconConstraints: BoxConstraints(
+                      minHeight: 40,
+                      minWidth: 40,
+                    ),
+
+                    contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+                  ),
+                  onSelected: (RentalStatus? icon) {
+                    setState(() {
+                      selectedStatus = icon;
+                    });
+                  },
+                  dropdownMenuEntries: RentalStatus.entries,
+                  leadingIcon: ImageIcon(selectedStatus?.icon),
+                ),
+                ElevatedButton(onPressed: openDialog, child: Text("Button")),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Future openDialog() => showDialog(
+    context: context,
+    builder: (context) => Dialog(
+
+      child: StatefulBuilder(
+        builder: (context, setDialogState) {
+          return SizedBox(
+            width: 300,
+            height: 400,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+
+                    setDialogState(() {
+                      warningList.add(TextEditingController());
+                    });
+                  },
+                  child: const Text("Add Field"),
+                ),
+
+
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: warningList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: warningList[index],
+                          decoration: InputDecoration(
+                            labelText: 'Field ${index + 1}',
+                            border: const OutlineInputBorder(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    ),
+  );
+  }
